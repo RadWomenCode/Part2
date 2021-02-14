1c48576d928bd778710160a830a03699761a23bd
Melissa Hansen <me@melissajhansen.com>
2021-02-13 17:37:30 -0800

    Start of recipe component and data loading

---
+++ force-app/main/default/lwc/recipeList/recipeList.js
@@ -0,0 +1,28 @@
+import { LightningElement, wire } from 'lwc';
+import BOOK_IMG from '@salesforce/resourceUrl/CookbookSilhouette';
+
+/** RecipeController.searchRecipes(searchTerm) Apex method */
+import searchRecipes from '@salesforce/apex/RecipeController.searchRecipes';
+export default class recipeList extends LightningElement {
+    // Expose the static resource URL for use in the template
+    bookSilhouette = BOOK_IMG;
+
+	searchTerm = '';
+	@wire(searchRecipes, {searchTerm: '$searchTerm'})
+	recipes = [];
+
+	handleSearchTermChange(event) {
+		// Debouncing this method: do not update the reactive property as
+		// long as this function is being called within a delay of 300 ms.
+		// This is to avoid a very large number of Apex method calls.
+		window.clearTimeout(this.delayTimeout);
+		const searchTerm = event.target.value;
+		// eslint-disable-next-line @lwc/lwc/no-async-operation
+		this.delayTimeout = setTimeout(() => {
+			this.searchTerm = searchTerm;
+		}, 300);
+	}
+	get hasResults() {
+		return (this.recipes.data.length > 0);
+	}
+}
